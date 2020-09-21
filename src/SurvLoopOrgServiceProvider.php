f25166a9be755c8892177c8e557c994e5527d6ce
Morgan Lesko <15270653+rockhopsoft@users.noreply.github.com>
2020-09-21 15:00:05 -0400

    Migration Fix

--- src/SurvLoopOrgServiceProvider.php
+++ src/SurvLoopOrgServiceProvider.php
@@ -12,17 +12,20 @@ class SurvLoopOrgServiceProvider extends ServiceProvider
             return new SurvLoopOrgFacade();
         });
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
+        $dbMig = '2020_09_14_000000_create_survloop_installs_tables';
         $this->publishes([
 
-              __DIR__ . '/Views'   => base_path('resources/views/vendor/survlooporg'),
+              __DIR__ . '/Views'
+                  => base_path('resources/views/vendor/survlooporg'),
 
-              __DIR__ . '/Models'  => base_path('app/Models'),
+              __DIR__ . '/Models'
+                  => base_path('app/Models'),
 
-              __DIR__ . '/Uploads' => base_path('storage/app/up/survlooporg'),
+              __DIR__ . '/Uploads' 
+                  => base_path('storage/app/up/survlooporg'),
               
-              __DIR__ . '/Database/2020_09_14_000000_create_survloop_installs_tables.php'
-                  => base_path('database/migrations/'
-                      . '2020_09_14_000000_create_survloop_installs_tables.php'),
+              __DIR__ . '/Database/' . $dbMig . '.php'
+                  => base_path('database/migrations/' . $dbMig . '.php'),
 
               __DIR__ .'/Database/SurvLoopOrgSeeder.php' 
                   => base_path('database/seeders/SurvLoopOrgSeeder.php')
